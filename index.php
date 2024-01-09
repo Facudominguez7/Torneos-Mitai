ab8e54a5af9335e44d915c57802b8ec997dfff1e
Facundo Diaz Dominguez <facudominguez457@gmail.com>
2024-01-09 15:13:44 -0300

    Se sacó el mensaje version php

--- index.php
+++ index.php
@@ -233,11 +233,6 @@ conectar();
                     </div>
                 </div>
             </div>
-
-            <?php
-                echo 'Versión de PHP ' . phpversion();
-            ?>
-
             <div class="flex justify-center flex-wrap mt-4">
                 <button class=" mb-4 middle none center mr-4 rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" data-ripple-light="true">
                     <a href="index.php?modulo=campeones">
